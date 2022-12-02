2f3ce083d1615417a9f978a0c964c3fbbf3f4032
Stenli777 <alekseysten777@yandex.ru>
2022-12-02 15:41:30 +0300

    add breadcrumbs

--- resources/views/asic.blade.php
+++ resources/views/asic.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.homepage')
 @section('main')
-{{--    {{ Breadcrumbs::render('asic',$asic) }}--}}
+    {{ Breadcrumbs::render('asic',$asic) }}
     <div class="container-fluid asic-back">
-        <p>test console</p>
     <div class="container asic-page">
         <h1>{{$asic->producer->name}} {{$asic->name}} {{$asic->humanHashrate()}}</h1>
         <div class="row">
