6bba353641edc9cc8d9fe6a3d2482acd76b124a2
Alexander Shishlev <ashishlev@vepay.online>
2022-12-18 19:52:17 +0300

    gerwin command

--- app/Http/Controllers/GerwinController.php
+++ app/Http/Controllers/GerwinController.php
@@ -11,7 +11,7 @@ class GerwinController extends Controller
         $task_id = $request->json('id');
         $model = Gerwin::query()->where('gerwin_id', $task_id)->firstOrFail();
 
-        $model->task_result = json_encode(var_export($request, 1));
+        $model->task_result = var_export($request->post(), 1);
         $model->task_status = $request->json('status');
         $model->save();
     }
