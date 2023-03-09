7d0bc203a63f5acf57a3962cea8de00d24faee22
Stenli777 <alekseysten777@yandex.ru>
2023-03-09 19:39:38 +0300

    edit post img

--- resources/views/blocks/article.blade.php
+++ resources/views/blocks/article.blade.php
@@ -1,7 +1,7 @@
 <div class="card mb-3" style="border-radius: 0">
     <div class="text-center" style="">
         <img href="{{$post->is_news === 1?'/new/':'/post/'}}{{$post->alias}}" class="card-img"
-             src="/{{$post->img ? $post->img : "images/uploads/asics/placeholder.png"}}"
+             src="/{{$post->img ? "storage/{$post->img}" : "images/uploads/asics/placeholder.png"}}"
              alt="изображение {{$post->title}}">
     </div>
     <div class="card-body pt-0 mt-3">
