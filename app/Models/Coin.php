7188df8cbed04d7390f32c96db3ce8817e488683
Stenli777 <alekseysten777@yandex.ru>
2023-05-03 10:30:50 +0300

    edit coins minePerDay & counter metrika

--- app/Models/Coin.php
+++ app/Models/Coin.php
@@ -18,7 +18,7 @@ class Coin extends Model
 
     public function minePerDay($hashrate){
         $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 32 * $this->wtm_coin->difficulty);
-        if (in_array($this->id,[32,10,11,67,81,85,107])) {
+        if (in_array($this->id,[32,10,11,67,81,85,107,111])) {
             $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / ($this->wtm_coin->difficulty);
         } if (in_array($this->id,[19,22,98])) {
             $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 13 * $this->wtm_coin->difficulty);
@@ -30,17 +30,8 @@ class Coin extends Model
 //        if (in_array($this->id,[32])) {
 //            $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 9 * $this->wtm_coin->difficulty);
 //        }
-        //flux
         if (in_array($this->id,[104])) {
-            $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 16 * $this->wtm_coin->difficulty);
-        }
-        //kadena
-        if (in_array($this->id,[11])) {
-            $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 26 * $this->wtm_coin->difficulty);
-        }
-        //monero
-        if (in_array($this->id,[111])) {
-            $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / ($this->wtm_coin->difficulty);
+            $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 12 * $this->wtm_coin->difficulty);
         }
         return $mine;
     }
