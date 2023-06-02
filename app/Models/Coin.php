216feeb161ce2bfbae1e0f3739ebf460c5f8f488
Stenli777 <alekseysten777@yandex.ru>
2023-06-02 11:46:39 +0300

    edit mine per day

--- app/Models/Coin.php
+++ app/Models/Coin.php
@@ -18,9 +18,9 @@ class Coin extends Model
 
     public function minePerDay($hashrate){
         $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 32 * $this->wtm_coin->difficulty);
-        if (in_array($this->id,[32,10,11,67,81,85,107,111,83])) {
+        if (in_array($this->id,[32,10,11,67,81,85,107,111,83,19])) {
             $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / ($this->wtm_coin->difficulty);
-        } if (in_array($this->id,[19,22,98])) {
+        } if (in_array($this->id,[22,98])) {
             $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 13 * $this->wtm_coin->difficulty);
         } if (in_array($this->id,[100])) {
             $mine = (86400 * $this->wtm_coin->block_reward * $hashrate) / (2 ** 1 * $this->wtm_coin->difficulty);
