8eec371cf2e39ca6a039339dc79aa7cb7758f197
misha <moroz.mkhl@gmail.com>
2024-08-25 02:35:04 +0600

    improvements for apps

--- resources/views/blocks/app.blade.php
+++ resources/views/blocks/app.blade.php
@@ -3,11 +3,11 @@
         <a href="/app/{{$post->alias}}">
         <img href="/app/{{$post->alias}}" class="card-img"
              src="/{{$post->img ? "storage/{$post->img}" : "images/uploads/asics/placeholder.png"}}"
-             alt="изображение {{$post->title}}">
+             alt="изображение {{$post->name}}">
         </a>
     </div>
     <div class="card-body pt-0 mt-3">
-        <p><a href="/app/{{$post->alias}}">{{$post->title}}</a></p>
+        <p><a href="/app/{{$post->alias}}">{{$post->name}}</a></p>
         <a href="/app/{{$post->alias}}">{{$post->publicDate()}}</a>
     </div>
 </div>
