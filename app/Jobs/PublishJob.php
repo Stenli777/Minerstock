1d5974eedf03694ec9ef6ef507b420b6ca593e71
misha <moroz.mkhl@gmail.com>
2024-09-09 13:17:50 +0600

    hotfix

--- app/Jobs/PublishJob.php
+++ app/Jobs/PublishJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\App;
 use App\Models\Post;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
@@ -21,7 +22,7 @@ class PublishJob implements ShouldQueue
      *
      * @return void
      */
-    public function __construct(Post $post)
+    public function __construct(Post|App $post)
     {
         $this->post = $post;
     }
@@ -31,7 +32,7 @@ class PublishJob implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle()git
     {
         if ($this->post->created_at <= now()) {
             event(new \App\Events\NewsPublished($this->post));
